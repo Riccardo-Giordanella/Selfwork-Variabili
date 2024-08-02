663e08ecf7463764dd6e040a8e4c73a6270e2bb6
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-02 22:38:19 +0200

    Fine dell'esercizio

---
+++ main.js
@@ -0,0 +1,22 @@
+// Dichiarazione della prima variabile
+let num1 = 10
+// Dichiarazione della seconda variabile
+let num2 = 20
+// Visualizzazione in console "Il valore della variabile num1 e' tot"
+console.log(`Il valore della variabile num1 è`, num1);
+// Visualizzazione in console "Il valore della variabile num2 e' tot"
+console.log(`Il valore della variabile num2 è`, num2);
+// Dichiarazione della variabile stringa
+let stringa = `Aulab`
+// Visualizzazione in console "Il valore della variabile stringa e' tot"
+console.log(`Il valore della variabile stringa è`, stringa);
+// Sostituzione della variabile stringa in altro valore e stampa su schermo
+stringa = `Javascript`
+console.log(`Il nuovo valore della variabile stringa è`, stringa);
+// Dichiarazione della costante Pigreco
+const PIGRECO = 3.14159265359
+console.log(`Il valore della costante PIGRECO è`, PIGRECO);
+// Nell'ultima task chiede di cambiare il valore alla costante PIGRECO, ovviamente tale cambiamento risulterebbe un errore in quanto una costante non può essere modificata nel tempo come le variabili ma procedo comunque a scriverlo in modo da tirar fuori l'errore dalla console
+PIGRECO = 4
+
+// Fine Esercizio
